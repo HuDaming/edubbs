59caf94539b1eebee43b64fdd46856c28f152787
Hudaming <40834106@qq.com>
2018-05-24 03:20:51 +0000

    Topic order

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -15,4 +15,50 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * 不同的排序，使用不同的数据读取逻辑
+     *
+     * @param $query
+     * @param $order
+     * @return mixed
+     */
+    public function scopeWithOrder($query, $order)
+    {
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+
+            default:
+                $query->recentReplied();
+                break;
+        }
+
+        // 预加载防止 N+1 问题
+        return $query->with('user', 'category');
+    }
+
+    /**
+     * sort by updated_at
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function scopeRecentReplied($query)
+    {
+        // 当话题有新回复时，模型的 reply_count 属性将会更新，此时会自动触发框架对数据模型 updated_at 时间戳的更新
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    /**
+     * sort by created_at
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function scopeRecent($query)
+    {
+        return $query->orderBy('created_at', 'desc');
+    }
 }
